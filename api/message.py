29a61fd6da307d1fc16ecc26ca8e7c4572a611fd
Israël Hallé <israel.halle@shopify.com>
2015-02-22 02:26:53 -0500

    Include source fake name in city message

--- api/message.py
+++ api/message.py
@@ -34,6 +34,8 @@ class Message(restful.Resource):
 
         if dst_user:
             data['participants'] = '{}:{}'.format(*sorted([self.user.id,dst_user]))
+        else:
+            data['src_fake_name'] = self.user.fake_name
 
         success = Event.create(data)
 
