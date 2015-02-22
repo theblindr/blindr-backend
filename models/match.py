c598f1c987bd54bb44c83c687e2b75bcbc5dd684
Israël Hallé <israel.halle@shopify.com>
2015-02-21 20:45:31 -0500

    Alter matches schema

--- models/match.py
+++ models/match.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, String, Integer, ForeignKey, Boolean, CHAR, TIMESTAMP
+from sqlalchemy import Column, String, Integer, ForeignKey, Boolean, CHAR, TIMESTAMP, PrimaryKeyConstraint
 from sqlalchemy.orm import relationship, backref
 
 Base = declarative_base()
@@ -7,8 +7,8 @@ Base = declarative_base()
 class Match(Base):
     __tablename__ = 'matches'
 
-    id =            	Column(Integer, primary_key=True)
-
     match_from_id = 	Column(Integer, ForeignKey('users.id'))
     match_to_id =	Column(Integer, ForeignKey('users.id'))
     mutual =		Column(Boolean)
+
+    __table_args__ = (PrimaryKeyConstraint('match_from_id', 'match_to_id'),)
