9b33dcddd9dc28e2ed460aa4108f847d762fc35d
Marc-Antoine Hinse <marc_antoine_hinse@hotmail.com>
2015-04-27 14:10:40 -0400

    We can now request all of the profile pictures
    Instead of fetching the first 5 ones, we need to add a logic to fetch the 5 pictures the user selected when setting the application

--- api/pictures.py
+++ api/pictures.py
@@ -14,20 +14,32 @@ class Pictures(restful.Resource):
     method_decorators=[authenticate]
 
     def get(self):
-        dst_id = request.args.get('dst_id')
-        match = config.session.query(Match).filter(
-            (Match.match_from_id == self.user.id) |
-            ((Match.match_to_id == self.user.id) & (Match.mutual == True)))
+        typeReq = request.args.get('typeReq')
+        
+        if typeReq == 'slideshow':
+            dst_id = request.args.get('dst_id')
+            match = config.session.query(Match).filter(
+                (Match.match_from_id == self.user.id) |
+                ((Match.match_to_id == self.user.id) & (Match.mutual == True)))
 
-        if match is not None: #matches found, serve pictures
-
-            target_user = config.session.query(User).filter(User.id==dst_id).first()
-            facebook.GraphAPI(target_user.OAuth)
-            graph = facebook.GraphAPI(target_user.OAuth)
-            target_user_albums = graph.get_connections(target_user.id, 'albums')['data']
+            if match is not None: #matches found, serve pictures
+                target_user = config.session.query(User).filter(User.id==dst_id).first()
+                facebook.GraphAPI(target_user.OAuth)
+                graph = facebook.GraphAPI(target_user.OAuth)
+                target_user_albums = graph.get_connections(target_user.id, 'albums')['data']
+                profile_pictures_album = target_user_albums[0]
+                profile_picture_album_id = profile_pictures_album['id']
+                urls = [x['images'][0]['source'] for x in graph.get_connections(profile_picture_album_id, 'photos')['data']][:5]
+                return urls
+            else:
+                abort(500)
+        elif typeReq == 'all':
+            facebook.GraphAPI(self.user.OAuth)
+            graph = facebook.GraphAPI(self.user.OAuth)
+            target_user_albums = graph.get_connections(self.user.id, 'albums')['data']
             profile_pictures_album = target_user_albums[0]
             profile_picture_album_id = profile_pictures_album['id']
-            urls = [x['images'][0]['source'] for x in graph.get_connections(profile_picture_album_id, 'photos')['data']][:5]
+            urls = [x['images'][0]['source'] for x in graph.get_connections(profile_picture_album_id, 'photos')['data']]
             return urls
         else:
             abort(500)
