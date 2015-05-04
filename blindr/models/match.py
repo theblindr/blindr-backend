d68193e54f764b69b6a2d408a69a5a4c75fac2f6
isra17 <isra017@gmail.com>
2015-05-04 19:58:24 -0400

    Refactor models.match

--- blindr/models/match.py
+++ blindr/models/match.py
@@ -1,13 +1,11 @@
-from sqlalchemy import Column, String, Integer, ForeignKey, Boolean, CHAR, TIMESTAMP, PrimaryKeyConstraint
-from sqlalchemy.orm import relationship, backref
 from blindr import db
 
 
 class Match(db.Model):
     __tablename__ = 'matches'
 
-    match_from_id = 	Column(String, ForeignKey('users.id'))
-    match_to_id =	Column(String, ForeignKey('users.id'))
-    mutual =		Column(Boolean)
+    match_from_id = 	db.Column(db.String, db.ForeignKey('users.id'))
+    match_to_id =	db.Column(db.String, db.ForeignKey('users.id'))
+    mutual =		db.Column(db.Boolean)
 
-    __table_args__ = (PrimaryKeyConstraint('match_from_id', 'match_to_id'),)
+    __table_args__ = (db.PrimaryKeyConstraint('match_from_id', 'match_to_id'),)
