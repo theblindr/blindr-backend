c6b66b6648ca9d42cd01db2862ce4c4022db9fa7
isra17 <isra017@gmail.com>
2015-05-06 00:02:35 -0400

    Add api.like tests

---
+++ tests/api/test_like.py
@@ -0,0 +1,68 @@
+from tests.blindrtest import BlindrTest
+from tests.factory_boy.match_factory import MatchFactory
+from tests.factory_boy.user_factory import UserFactory
+from blindr.models.match import Match
+from unittest import mock
+import json
+
+class LikeTest(BlindrTest):
+
+    def test_post_first_like(self):
+        UserFactory(id='user2')
+
+        rv = self.auth_post('/events/like', data=dict(dst_user='user2'))
+
+        rv_data = json.loads(rv.data.decode('utf-8'))
+        self.assert_200(rv)
+
+        match = Match.query.get((self.auth_user.id,'user2'))
+        self.assertIsNotNone(match)
+        self.assertFalse(match.mutual)
+
+    @mock.patch('blindr.api.like.Event.create')
+    def test_post_match(self, mock_create_event):
+        MatchFactory(
+            match_from_id=UserFactory(id='user2').id,
+            match_to_id=self.auth_user.id,
+            mutual=False
+        )
+
+        rv = self.auth_post('/events/like', data=dict(dst_user='user2'))
+
+        rv_data = json.loads(rv.data.decode('utf-8'))
+        self.assert_200(rv)
+
+        match = Match.query.get(('user2',self.auth_user.id))
+        self.assertIsNotNone(match)
+        self.assertTrue(match.mutual)
+
+        mock_create_event.assert_any_call({
+            'type': 'match',
+            'participants': 'auth_user:user2',
+            'dst': 'user:user2',
+            'src': 'auth_user',
+            'src_real_name': 'Auth User',
+            'src_fake_name': 'Foo Bar'
+        })
+
+        mock_create_event.assert_any_call({
+            'type': 'match',
+            'participants': 'auth_user:user2',
+            'dst': 'user:auth_user',
+            'src': 'user2',
+            'src_real_name': 'Realname',
+            'src_fake_name': 'Fakename'
+        })
+
+    def test_like_unvalid_user(self):
+        rv = self.auth_post('/events/like', data=dict(dst_user='foobar'))
+        self.assert_status(rv, 422)
+
+    def test_like_auth(self):
+        rv = self.client.post('/events/like', data=dict(dst_user='user2'))
+        self.assert_401(rv)
+
+        rv = self.client.get('/events/like')
+        self.assert_401(rv)
+
+
