009f587be302964dc5d795b6a790d838f277d9c6
Marc-Antoine Hinse <marc_antoine_hinse@hotmail.com>
2015-05-10 16:41:11 -0400

    Modify tests

--- tests/api/test_pictures.py
+++ tests/api/test_pictures.py
@@ -6,19 +6,6 @@ import json
 class PicturesTest(BlindrTest):
 
     @mock.patch('blindr.models.user.facebook.GraphAPI')
-    def test_get_all_pictures(self, mock_graphapi):
-        graph_instance = mock_graphapi.return_value
-        graph_instance.get_connections.side_effect = [
-            {'data':[{'id':'album_123123'}]},
-            {'data':[{'images':[{'source':'http://image.url'}]}]}
-        ]
-
-        rv = self.auth_get('/pictures', query_string=dict(typeReq='all'))
-
-        self.assert_200(rv)
-        rv_data = json.loads(rv.data.decode('utf-8'))
-
-        self.assertEqual(['http://image.url'], rv_data)
 
     def test_get_slideshow(self):
         UserFactory(
@@ -26,7 +13,7 @@ class PicturesTest(BlindrTest):
             facebook_urls='http://image.url,http://foobar.com'
         )
 
-        rv = self.auth_get('/pictures', query_string=dict(typeReq='slideshow', dst_id='user2'))
+        rv = self.auth_get('/pictures', query_string=dict(dst_id='user2'))
 
         self.assert_200(rv)
         rv_data = json.loads(rv.data.decode('utf-8'))
