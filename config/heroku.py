4a8f3bd478a3a942efb1ecc1be7d01b1b82ae137
isra17 <isra017@gmail.com>
2015-05-10 10:26:45 -0400

    Add rollbar to config

--- config/heroku.py
+++ config/heroku.py
@@ -4,3 +4,4 @@ ENV = 'heroku'
 SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL')
 SECRET = environ.get('SECRET')
 AUTH_SECRET = environ.get('AUTH_SECRET')
+ROLLBAR_ACCESS_TOKEN = environ.get('ROLLBAR_ACCESS_TOKEN')
