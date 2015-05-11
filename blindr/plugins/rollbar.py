1051d6ebbb13e0950feb70a370f892b372c32ea7
Israel Halle <isra017@gmail.com>
2015-05-10 22:15:44 -0400

    Set http exception to warning

--- blindr/plugins/rollbar.py
+++ blindr/plugins/rollbar.py
@@ -4,13 +4,18 @@ import rollbar.contrib.flask
 import rollbar
 import os
 
+import werkzeug.exceptions
+
 def init_rollbar(app):
     if 'ROLLBAR_ACCESS_TOKEN' in app.config:
         rollbar.init(
             app.config['ROLLBAR_ACCESS_TOKEN'],
             app.config['ENV'],
             root=os.path.dirname(os.path.realpath(blindr.__file__)),
-            allow_logging_basic_config=False
+            allow_logging_basic_config=False,
+            exception_level_filters=[
+                (werkzeug.exceptions, 'warning'),
+            ]
         )
 
         got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
