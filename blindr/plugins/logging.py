c79ebc91bcd2a9c6b0257485b001993ab3cc0f15
Israel Halle <isra017@gmail.com>
2015-05-10 21:54:55 -0400

    Remove logentries

--- blindr/plugins/logging.py
+++ blindr/plugins/logging.py
@@ -1,7 +1,5 @@
-import logentries
 import logging
 
 def init_logging(app):
     app.logger.addHandler(logging.StreamHandler())
-    app.logger.addHandler(logentries.LogentriesHandler('25ad6a50-0ab3-41d8-ab1c-416904d00992'))
     app.logger.setLevel(logging.INFO)
