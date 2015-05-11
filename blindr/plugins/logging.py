eec05fb6a1e9a80fc3577af33884dd0ef960973e
Israel Halle <isra017@gmail.com>
2015-05-10 21:30:12 -0400

    Add logentries logging

--- blindr/plugins/logging.py
+++ blindr/plugins/logging.py
@@ -1,6 +1,7 @@
+from logentries
 import logging
 
 def init_logging(app):
-    stream_handler = logging.StreamHandler()
-    app.logger.addHandler(stream_handler)
+    app.logger.addHandler(logging.StreamHandler())
+    app.logger.addHandler(logentries.LogentriesHandler('25ad6a50-0ab3-41d8-ab1c-416904d00992'))
     app.logger.setLevel(logging.INFO)
