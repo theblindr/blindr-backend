bf93f75bf481f1be33c49357a417680ec1495d91
isra17 <isra017@gmail.com>
2015-05-12 00:59:38 -0400

    Complete create_db script

--- config/development.py
+++ config/development.py
@@ -1,11 +1,12 @@
 import logging
+import os
 
 DEBUG = True
 SQLALCHEMY_ECHO = True
 
 LOGGING_ENABLED = False
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///instance/blindr.db'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///{}/instance/blindr.db'.format(os.getcwd())
 
 import boto
 boto.config.load_from_path('instance/boto.cfg')
